b387b1600ac63ddbec762904df2c64dd640ce7ea
paja <paja@hamt.eu>
2019-12-03 23:28:57 +0100

    additional second layer keys

--- keyboards/handwired/bluepill/keymaps/default/keymap.c
+++ keyboards/handwired/bluepill/keymaps/default/keymap.c
@@ -31,7 +31,7 @@ enum custom_keycodes {
   QMKBEST = SAFE_RANGE,
   QMKURL,
   SFT_ESC,
-  KEYLOCK
+  KEYLOCK,
 };
 
 
@@ -42,11 +42,11 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRAVE, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL,     KC_BSPC,      KC_DELETE, \
       KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,    KC_BSLASH,    KC_PGUP, \
      KC_CAPS, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,           KC_ENT,         KC_PGDOWN, \
-     KC_LSPO, KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,     KC_RSPC,     KC_UP,   TT(2), \
+     KC_LSPO, KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,     KC_RSPC,     KC_UP,   TG(1), \
      KC_LCTL, KC_LGUI,KC_LALT,                        KC_SPC,                  KC_RALT,   MO(1),    KC_RCTL,   KC_LEFT, KC_DOWN, KC_RIGHT ),
 
   [_FNONE] = LAYOUT_seventy_ansi(
-    KEYLOCK, KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+    KEYLOCK, DM_REC1, DM_REC2, KC_MPLY, KC_MNXT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_POWER, \
     DM_RSTP, DM_PLY1, DM_PLY2, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P7,   KC_P8,   KC_P9,   KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,     KC_MUTE, \
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_TRNS, KC_TRNS,      KC_TRNS,     KC_VOLU, \
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P1,   KC_P2,   KC_P3,   KC_PAST, KC_TRNS,             KC_TRNS,       KC_VOLD, \
@@ -54,8 +54,8 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TRNS,                  KC_TRNS,                          KC_TRNS,   KC_TRNS,  KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS ),
 
   [_FNTWO] = LAYOUT_seventy_ansi(
-    KEYLOCK, KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-    KC_TRNS, DYN_REC_START1, DM_REC2, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P7,   KC_P8,   KC_P9,   KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,     KC_MUTE, \
+    KEYLOCK, KC_TRNS, KC_TRNS, KC_MPLY, KC_MNXT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P7,   KC_P8,   KC_P9,   KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,     KC_MUTE, \
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_TRNS, KC_TRNS,      KC_TRNS,     KC_VOLU, \
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P1,   KC_P2,   KC_P3,   KC_PAST, KC_TRNS,             KC_TRNS,       KC_VOLD, \
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P0,   KC_PDOT, KC_PENT, KC_PSLS,      KC_TRNS,     KC_TRNS, KC_TRNS, \
@@ -125,12 +125,12 @@ void matrix_init_user(void) {
 layer_state_t layer_state_set_user(layer_state_t state) {
   switch (get_highest_layer(state)) {
   case _FNONE:
-    palSetPad(GPIOA, 0);  //OFF Color A
-    palClearPad(GPIOA, 1); //ON Color B
-    break;
-  case _FNTWO:
     palClearPad(GPIOA, 0); //ON Color A
     palSetPad(GPIOA, 1);  //ON Color B
+    break;
+  case _FNTWO:
+    palSetPad(GPIOA, 0);  //OFF Color A
+    palClearPad(GPIOA, 1); //ON Color B
     break;
   default: //  for any other layers, or the default layer
     if (keys_locked) {
@@ -143,4 +143,14 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     break;
   }
   return state;
+}
+
+void suspend_power_down_user(void) {
+  palClearPad(GPIOA, 0); //ON Color A
+  palClearPad(GPIOA, 1);  //OFF Color B
+}
+
+void suspend_wakeup_init_user(void) {
+  palSetPad(GPIOA, 0); //ON Color A
+  palSetPad(GPIOA, 1);  //OFF Color B
 }
